b136bcb6ef60c22599021f364e0cf3eab5dce206
Dante-1993 <73445345+Dante-1993@users.noreply.github.com>
2026-02-27 17:41:14 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -11,7 +11,7 @@ pkg install -y \
 su -c "mkdir -p /data/data/com.termux/files/usr/var/lxc/debian/rootfs \
       cd /data/local/lxc/debian/rootfs
       busybox wget https://github.com/Dante-1993/TermuxChroot-Autoinstall/releases/download/RootFS/debian-arm64.tar.gz 
-      tar xpvf debian-arm64.tar.gz -C ."
+      tar xpvf debian-arm64.tar.gz -C /data/data/com.termux/files/usr/var/lxc/debian/rootfs"
 sudo cp config /data/data/com.termux/files/usr/var/lxc
 chmod +x start.sh
 cp -v start.sh ~/.shortcuts
