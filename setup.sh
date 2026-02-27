53effd35ffb90f8ae17e7fdfee8f91db7c6d29b9
Dante-1993 <73445345+Dante-1993@users.noreply.github.com>
2026-02-27 18:13:43 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -6,15 +6,14 @@ pkg install -y \
   curl \
   proot \
   pulseaudio \
-  termux-x11-nightly 
-su -c "mkdir -p /data/local/lxc
-      cd /data/local/lxc
-      /data/data/com.termux/files/usr/bin/wget https://github.com/termux-containers/lxc-android/releases/download/v5.0.3/lxc-android-aarch64.tar.xz
-      tar -xJf lxc-android-aarch64.tar.xz "
+  termux-x11-nightly \
+  lxc
 su -c "mkdir -p /data/local/lxc/debian/rootfs \
       cd /data/local/lxc/debian/rootfs
       busybox wget https://github.com/Dante-1993/TermuxChroot-Autoinstall/releases/download/RootFS/debian-arm64.tar.gz 
       tar xpvf debian-arm64.tar.gz -C /data/local/lxc/debian/rootfs"
 sudo cp config /data/local/lxc/debian
+sudo lxc-create -n debian -t local \
+  -- --rootfs-dir=/data/local/lxc/debian/rootfs
 chmod +x start.sh
 cp -v start.sh ~/.shortcuts
