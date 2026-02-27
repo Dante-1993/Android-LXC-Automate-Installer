5024322bac31a03551172943f255bb30c13cabd3
Dante-1993 <73445345+Dante-1993@users.noreply.github.com>
2026-02-27 17:34:43 +0200

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,17 @@
+pkg install -y \
+  root-repo \
+  x11-repo \
+  tsu \
+  wget \
+  curl \
+  proot \
+  pulseaudio \
+  termux-x11-nightly \
+  lxc
+su -c "mkdir -p /data/data/com.termux/usr/var/lxc/debian/rootfs \
+      cd /data/local/lxc/debian/rootfs
+      busybox wget https://github.com/Dante-1993/TermuxChroot-Autoinstall/releases/download/RootFS/debian-arm64.tar.gz 
+      tar xpvf debian-arm64.tar.gz -C ."
+sudo cp config /data/data/com.termux/usr/var/lxc
+chmod +x start.sh
+cp -v start.sh ~/.shortcuts
