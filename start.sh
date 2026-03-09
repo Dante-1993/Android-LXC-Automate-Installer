32ec2e296bfbc482a8dfbb5ed834afc86b3e53c4
Dante-1993 <73445345+Dante-1993@users.noreply.github.com>
2026-03-10 00:45:56 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,1 +1,6 @@
+sudo mount -t tmpfs -o mode=0755 cgroup /sys/fs/cgroup
+sudo mkdir /sys/fs/cgroup/cpu /sys/fs/cgroup/memory /sys/fs/cgroup/devices
+sudo mount -t cgroup -o cpu cgroup /sys/fs/cgroup/cpu
+sudo mount -t cgroup -o memory cgroup /sys/fs/cgroup/memory
+sudo mount -t cgroup -o devices cgroup /sys/fs/cgroup/devices
 sudo lxc-start -n debian -f /data/local/lxc/debian/config -F
