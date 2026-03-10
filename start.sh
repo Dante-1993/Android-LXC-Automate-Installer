962fff6628e74d426d14da1507872f0cc6f25079
Dante-1993 <73445345+Dante-1993@users.noreply.github.com>
2026-03-10 08:43:19 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,11 +2,11 @@
 sudo umount -l /sys/fs/cgroup
 
 #Додаємо cgroup v1
-sudo mount -t tmpfs -o mode=0755 cgroup /sys/fs/cgroup
-sudo mkdir /sys/fs/cgroup/cpu /sys/fs/cgroup/memory /sys/fs/cgroup/devices
-sudo mount -t cgroup -o cpu cgroup /sys/fs/cgroup/cpu
-sudo mount -t cgroup -o memory cgroup /sys/fs/cgroup/memory
-sudo mount -t cgroup -o devices cgroup /sys/fs/cgroup/devices
+udo mount -t tmpfs -o mode=0755 cgroup /sys/fs/cgroup
+#sudo mkdir /sys/fs/cgroup/cpu /sys/fs/cgroup/memory /sys/fs/cgroup/devices
+#sudo mount -t cgroup -o cpu cgroup /sys/fs/cgroup/cpu
+#sudo mount -t cgroup -o memory cgroup /sys/fs/cgroup/memory
+#sudo mount -t cgroup -o devices cgroup /sys/fs/cgroup/devices
 
 # Створюємо ієрархію (LXC це любить)
 sudo mkdir /sys/fs/cgroup/unified
