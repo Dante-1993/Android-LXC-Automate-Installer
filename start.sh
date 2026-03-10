5ead1d3506b8f85b4e1cb99e9ecbbeca67136762
Dante-1993 <73445345+Dante-1993@users.noreply.github.com>
2026-03-10 07:51:19 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,18 @@
+# Очищаємо старі маунти, якщо вони є
+sudo umount -l /sys/fs/cgroup
+
+#Додаємо cgroup v1
 sudo mount -t tmpfs -o mode=0755 cgroup /sys/fs/cgroup
 sudo mkdir /sys/fs/cgroup/cpu /sys/fs/cgroup/memory /sys/fs/cgroup/devices
 sudo mount -t cgroup -o cpu cgroup /sys/fs/cgroup/cpu
 sudo mount -t cgroup -o memory cgroup /sys/fs/cgroup/memory
 sudo mount -t cgroup -o devices cgroup /sys/fs/cgroup/devices
+
+# Створюємо ієрархію (LXC це любить)
+sudo mkdir /sys/fs/cgroup/unified
+sudo mount -t cgroup2 none /sys/fs/cgroup/unified
+
+# Якщо твоє ядро підтримує v1 (cpu, mem), можна додати і їх, 
+# але для початку вистачить і unified.
+
 sudo lxc-start -n debian -f /data/local/lxc/debian/config -F
